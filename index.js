56d510a6d40cad8bdb5fb84289cdc1cd6d7f91ff
abc <sairaj0@bitbucket.org>
2019-11-24 22:40:42 +0530

    login page authentication

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const dotenv  = require('dotenv')
+//import route
+const authRoute = require('./routes/auth')
+
+
+
+dotenv.config()
+
+//connect to db
+mongoose.connect(process.env.DB_CONNECT, 
+{ useNewUrlParser: true },
+()=>console.log('Connected to db'))
+
+//middleware
+app.use(express.json());
+
+
+
+//route middleware
+app.use('/api/user', authRoute)
+
+
+app.listen(3000, () => console.log('Server is running'));   
